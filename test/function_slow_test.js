46add64667cc0a8723a98af79b27302fb61698ea
Mike Lay <mike@mkly.io>
2020-04-04 14:21:42 -0700

    Add slow tests

---
+++ test/function_slow_test.js
@@ -0,0 +1,14 @@
+const { equal } = require("assert")
+const { one, two, three } = require("../src/functions")
+const { map } = Array.prototype
+
+describe("Function Slow tests", () =>
+  map.call([
+    it("one()", done =>
+      map.call([
+        setTimeout.bind(null, done, 1000),
+        equal.bind(null, one(), 1)], x => x())),
+    it("two()", done =>
+      map.call([
+        setTimeout.bind(null, done, 500),
+        equal.bind(null, two(), 2)], x => x()))], x => x))
